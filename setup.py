1eaad4fc9ba31a59564c2b26f7d212a56b96da98
hp0404 <hryhorii.pavlenko@gmail.com>
2021-03-04 15:53:47 +0200

    update url

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='scite',
     version='1.0.0',
-    url='https://github.com/hcss-utils/scite-wrapper.git',
+    url='https://github.com/hcss-utils/scite.git',
     author='HTU',
     author_email='hcssteamukraine@gmail.com',
-    description='scite_ pythons wrapper',
+    description='scite_ API in Python',
     packages=find_packages(),    
 )
