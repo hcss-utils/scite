86fa62ceda2678ada848132f8bd4df520fc2f4a5
hp0404 <hryhorii.pavlenko@gmail.com>
2021-03-05 21:13:09 +0200

    added sleep_and_retry decorator

--- scite/http_utils.py
+++ scite/http_utils.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 import requests
-from ratelimit import limits
+from ratelimit import limits, sleep_and_retry
 
 
+@sleep_and_retry
 @limits(calls=40, period=60)
 def remote_call(
-        endpoint: str, 
-        url: str = "https://api.scite.ai", 
-        payload: dict = {},
-    ):
+    endpoint: str,
+    url: str = "https://api.scite.ai",
+    payload: dict = {},
+    params: dict = {},
+):
     """Make a generic GET request with ratelimits applied."""
-    r = requests.get(f"{url}/{endpoint}", json=payload)
+    r = requests.get(f"{url}/{endpoint}", json=payload, params=params)
     r.encoding = "UTF-8"
     return r.json()
-
