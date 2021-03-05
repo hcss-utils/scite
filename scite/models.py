1dac884d9e7bee326b9c106ca04f5304045eb189
hp0404 <hryhorii.pavlenko@gmail.com>
2021-03-05 17:45:22 +0200

    Add Reference model

--- scite/models.py
+++ scite/models.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
-from typing import Any, AnyStr, Dict, List
+from typing import Any, AnyStr, Dict, List, NamedTuple
+import pandas as pd
 
 
 DOI = AnyStr
 DOIs = List[DOI]
 JSON = Dict[str, Any]
+Reference = NamedTuple(
+    "References", [
+        ("target_doi", DOI), 
+        ("concise_references", list),
+        ("detailed_references", pd.DataFrame)
+    ]
+)
